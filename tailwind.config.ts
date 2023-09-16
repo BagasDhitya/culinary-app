17640ff54e76a3ca1f57b6e4c74797fb61dca8e2
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-09-16 19:50:15 +0700

    add: color config and added landing illustration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'culinary-white': '#fafafa',
+        'culinary-amber-400': '#fbbf24',
+        'culinary-amber-500': '#f59e0b',
+        'culinary-amber-600': '#d97706',
+        'culinary-amber-700': '#b45309'
+      }
     },
   },
   plugins: [],
