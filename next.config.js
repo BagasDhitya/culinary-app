c4119dc17f97dc8465dabb1a6935dd9484d02d05
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-09-17 12:26:26 +0700

    add: setup env and next js config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+        images: {
+          domains: ['i.pinimg.com', 'spoonacular.com', 'webknox.com']
+        }
+}
 
 module.exports = nextConfig
